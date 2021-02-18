89ccfa22d5319cc8a9a54dded32be8df354b84d0
Alex <alexsandro.sousa12366@gmail.com>
2021-02-18 13:58:30 -0300

    filtros

--- lib/transacao.ex
+++ lib/transacao.ex
@@ -4,11 +4,19 @@ defmodule Transacao do
   @transacoes "transacoes.txt"
 
   def gravar(tipo, de, valor, data, para \\ nil) do
-    {:ok, binario} = File.read(@transacoes)
-    transacoes = binario
-    |> :erlang.binary_to_term()
-    transacoes = transacoes ++
+    transacoes = busca_transacoes() ++
      [%__MODULE__{tipo: tipo, de: de, valor: valor, data: data, para: para}]
-     transacoes
+     File.write(@transacoes, :erlang.term_to_binary(transacoes))
+  end
+
+  def busca_todas(), do: busca_transacoes()
+  def busca_por_ano(ano), do: Enum.filter(busca_transacoes(), &(&1.data.year == ano))
+  def busca_por_mes(ano, mes), do: Enum.filter(busca_transacoes(), &(&1.data.year == ano && &1.data.month == mes))
+
+    defp busca_transacoes() do
+      {:ok, binario} = File.read(@transacoes)
+
+      binario
+      |> :erlang.binary_to_term()
   end
 end
